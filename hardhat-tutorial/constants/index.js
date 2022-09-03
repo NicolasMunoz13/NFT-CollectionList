eebffbc462fe5e7f5207c4f5b62af7dea241844f
nicolas-munoz01 <98959106+nicolas-munoz01@users.noreply.github.com>
2022-09-02 21:49:54 -0500

    Adding NFT collection

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x98f5c62F7b5Bc735a64942dBec9456954c7a368A";
+const WHITELIST_CONTRACT_ADDRESS = "0x951271A2772569c7A1f39ba38718DdAd1AAEd4E4";
 // URL for the Crypto Devs NFT Metadata
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -582,4 +582,4 @@ export const abi = [
   },
 ];
 export const NFT_CONTRACT_ADDRESS =
-  "0x6805a66179283Fa1FcEc8D89f4e162bebe5C0617";
+  "0x951271A2772569c7A1f39ba38718DdAd1AAEd4E4";
